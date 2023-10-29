8eb48de95041c4b45845167b1bbc02d39105e39a
vagrant <vagrant@hellocloud-dev-box>
2023-10-29 14:39:47 +0000

    Check the output of launch template

--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,14 @@ output "availability_zone" {
   description = "Public URL for Application Load Balancer"
   value       = data.aws_availability_zones.available.names
 }
+
+# Launch Template Outputs
+output "launch_template_id" {
+  description = "Launch Template ID"
+  value = aws_launch_template.launch_template_meow_new.id
+}
+
+output "launch_template_latest_version" {
+  description = "Launch Template Latest Version"
+  value = aws_launch_template.launch_template_meow_new.latest_version
+}
