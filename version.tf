6ddc02f7a5c1c29ef1e36c0a982a3431e0e70498
vagrant <vagrant@hellocloud-dev-box>
2023-10-29 13:15:18 +0000

    Modify version.tf

--- version.tf
+++ version.tf
@@ -8,8 +8,5 @@ terraform {
 }
 
 provider "aws" {
-  shared_config_files      = ["/home/vagrant/.aws/config"]
-  shared_credentials_files = ["/home/vagrant/.aws/credentials"]
-  profile                  = "bca-aws-master-hc-dev"
   region                   = var.bca-aws-master-region
 }
